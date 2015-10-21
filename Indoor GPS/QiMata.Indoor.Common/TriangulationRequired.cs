7f4f34ab9aed55114e5ae1f1fb4f8a215b25be53
Jared Rhodes <jrhodes@qimata.com>
2015-10-21 18:22:55 -0400

    added one real file

---
+++ Indoor GPS/QiMata.Indoor.Common/TriangulationRequired.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace QiMata.Indoor.Common
+{
+    public class TriangulationRequired
+    {
+        public List<InitialDistance> InitialDistances { get; set; }
+
+        public Guid CashRegisterGuid { get; set; }
+
+
+        public class InitialDistance
+        {
+            public Guid Beacon1 { get; set; }
+            public Guid Beacon2 { get; set; }
+            
+            public int Distance { get; set; }
+        }
+    }
+}
