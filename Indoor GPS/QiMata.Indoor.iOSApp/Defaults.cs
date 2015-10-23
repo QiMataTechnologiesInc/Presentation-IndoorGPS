f0d661655fb2f0577a9a543a7b2b504462d2d672
Jared Rhodes <jrhodes@qimata.com>
2015-10-23 15:21:59 -0400

    syncing with marshall's comp

--- Indoor GPS/QiMata.Indoor.iOSApp/Defaults.cs
+++ Indoor GPS/QiMata.Indoor.iOSApp/Defaults.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Foundation;
+using QiMata.Indoor.Common;
 
 namespace QiMata.Indoor.iOSApp
 {
@@ -39,5 +40,31 @@ namespace QiMata.Indoor.iOSApp
         {
             get { return defaultPower; }
         }
+
+        public static List<Beacon> Beacons = new List<Beacon>
+        {
+            new Beacon
+            {
+                Uuid = Guid.Parse("E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"),
+                Major = 0,
+                Minor = 0
+            },
+            new Beacon
+            {
+                Uuid = Guid.Parse("e20a39f4-73f5-4bc4-a12f-17d1ad07a961"),
+                Major = 0,
+                Minor = 2
+            },
+            new Beacon
+            {
+                Uuid = Guid.Parse("636f3f8f-6491-4bee-95f7-d8cc64a863b5"),
+                Major = 0,
+                Minor = 1
+            },
+            //new Beacon
+            //{
+
+            //}
+        };
     }
 }
--- Indoor GPS/QiMata.Indoor.iOSApp/MainViewController.cs
+++ Indoor GPS/QiMata.Indoor.iOSApp/MainViewController.cs
@@ -1,36 +1,81 @@
-
 using System;
+using System.Collections.Concurrent;
 using System.Drawing;
 using CoreLocation;
 using Foundation;
 using UIKit;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using QiMata.Indoor.Common;
 
 namespace QiMata.Indoor.iOSApp
 {
     public partial class MainViewController : UIViewController
     {
-        List<CLBeacon>[] beacons;
-        CLLocationManager locationManager;
-        List<CLBeaconRegion> rangedRegions;
+        private List<CLBeacon>[] beacons;
+        private CLLocationManager locationManager;
+        private List<CLBeaconRegion> rangedRegions;
+        private Task _uploadTask;
+        private ConcurrentQueue<BeaconDistance> _beaconDistances;
 
-        static bool UserInterfaceIdiomIsPhone
+        private static bool UserInterfaceIdiomIsPhone
         {
             get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
         }
+
+
+
 
         public MainViewController()
             : base(UserInterfaceIdiomIsPhone ? "MainViewController_iPhone" : "MainViewController_iPad", null)
         {
             locationManager = new CLLocationManager();
             locationManager.DidRangeBeacons += HandleDidRangeBeacons;
+            _beaconDistances = new ConcurrentQueue<BeaconDistance>();
+
         }
 
         private void HandleDidRangeBeacons(object sender, CLRegionBeaconsRangedEventArgs e)
         {
             foreach (CLBeacon beacon in e.Beacons)
             {
-                Console.WriteLine(beacon.Major);
+                _beaconDistances.Enqueue(new BeaconDistance
+                {
+                    Distance = Convert.ToDouble(Math.Pow(10d, (-55 - beacon.Rssi)/20)),
+                    Beacon = Defaults.Beacons.Single(x => Guid.Parse(GetGuid(beacon)) == x.Uuid && beacon.Major.Int32Value == x.Major && beacon.Minor.Int32Value == x.Minor)
+                });
+                Console.WriteLine(
+                    $"Distance: {Convert.ToDecimal(Math.Pow(10d, (-55d - beacon.Rssi)/20))} | Major: {beacon.Major} | Minor: {beacon.Minor} | Rssi: {beacon.Rssi}");
+
+            }
+
+            if (_uploadTask == null || _uploadTask.IsCompleted)
+            {
+                if (!_beaconDistances.IsEmpty)
+                {
+                    _uploadTask = UploadBeacons();
+                }
+            }
+        }
+
+        private string GetGuid(CLBeacon beacon)
+        {
+            var notGuid = beacon.ProximityUuid.ToString();
+            return notGuid.Substring(notGuid.IndexOf("> ") + 2);
+        }
+
+        private async Task UploadBeacons()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://indoorgps.azurewebsites.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                var response = await client.PostAsJsonAsync("api/BeaconData", _beaconDistances.AsEnumerable());
             }
         }
 
@@ -41,35 +86,68 @@ namespace QiMata.Indoor.iOSApp
 
             // Release any cached data, images, etc that aren't in use.
         }
-
+        
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-
+            
             // Populate the regions we will range once.
             rangedRegions = new List<CLBeaconRegion>();
-
+            
             foreach (NSUuid uuid in Defaults.SupportedProximityUuids)
             {
                 CLBeaconRegion region = new CLBeaconRegion(uuid, uuid.AsString());
                 rangedRegions.Add(region);
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://indoorgps.azurewebsites.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.PutAsJsonAsync("api/BeaconData", new TriangulationRequired
+                {
+                    InitialDistances = new List<InitialDistance>
+                    {
+                        new InitialDistance
+                        {
+                            Beacon1 = 
+                        },
+                        new InitialDistance
+                        {
+
+                        },
+                        new InitialDistance
+                        {
+
+                        },
+                        //new InitialDistance
+                        //{
+
+                        //}
+                    }
+                });
             }
         }
 
         public override void ViewWillAppear(bool animated)
         {
             base.ViewWillAppear(animated);
-
             foreach (CLBeaconRegion region in rangedRegions)
+            {
                 locationManager.StartRangingBeacons(region);
+            }
         }
-
+        
         public override void ViewWillDisappear(bool animated)
         {
             base.ViewWillDisappear(animated);
 
             foreach (CLBeaconRegion region in rangedRegions)
+            {
                 locationManager.StopRangingBeacons(region);
+            }
         }
     }
 }
